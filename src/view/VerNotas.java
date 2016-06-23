f6433818d7a820c681d170632a1cb62018532c7f
iftmjava <mefefet@top1post.ru>
2016-06-23 17:59:22 -0300

    nem sei que att
    xD

--- src/view/VerNotas.java
+++ src/view/VerNotas.java
@@ -34,11 +34,11 @@ public class VerNotas extends javax.swing.JDialog {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 810, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 480, Short.MAX_VALUE)
         );
 
         pack();
